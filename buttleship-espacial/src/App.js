b34ae3ca640a4828733138f07ccdb832e75a627f
CarolinaMedin <carolina.medina@usach.cl>
2020-01-20 00:15:44 -0300

    creación objeto nave

--- buttleship-espacial/src/App.js
+++ buttleship-espacial/src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Spaceship from './Spaceship';
+// import './App.css';
 
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      
+
+const initialState = {
+  // food: getRandomCoordinates(),
+  // speed: 200,
+  // direction: 'RIGHT',
+  spaceShip1: [  // coordenadas de game-Board, pasos de 10%
+    [0,0],
+    4,
+    "X"
+  ],
+  spaceShip2: [  // coordenadas de game-Board, pasos de 10%
+    [50,90],
+    [60,90]
+  ]
+}
+
+
+
+class App extends Component {
+
+  state = initialState;
+
+
+ render() {
+    return (
+      <div>
+      <div className="game-Board">
+        <Spaceship spaceShip={this.state.spaceShip1}/>
+        // <Spaceship spaceShip={this.state.spaceShip2}/>
+      </div>
+      <div className="game-Board">
+      </div>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;

1313167ff0a541171fa415193505085b0a21cf77
CarolinaMedin <carolina.medina@usach.cl>
2020-01-25 00:50:10 -0300

    cambiando nombre variables, y resumiendo códigos

--- buttleship-espacial/src/App.js
+++ buttleship-espacial/src/App.js
@@ -10,19 +10,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="game-Board">
-          <SpaceShip />
-          <SpaceShip state={{posicion_X: 50, posicion_Y: 50 ,largo: 3, orientacion: "Y"}}/>
-          <SpaceShip state={{posicion_X: 20, posicion_Y: 70 ,largo: 2, orientacion: "X"}}/>
-          <SpaceShip state={{posicion_X: 90, posicion_Y: 90 ,largo: 1, orientacion: "X"}}/>
+        <div className="game-Board">
+            <SpaceShip />
+            <SpaceShip state={{coordenada_X: 50, coordenada_Y: 50 ,largo: 3, orientacion: "V"}}/>
+            <SpaceShip state={{coordenada_X: 20, coordenada_Y: 70 ,largo: 2, orientacion: "H"}}/>
+            <SpaceShip state={{coordenada_X: 90, coordenada_Y: 90 ,largo: 1, orientacion: "H"}}/>
+        </div>
+        <div className="game-Board">
+            <SpaceShip state={{coordenada_X: 20, coordenada_Y: 20 ,largo: 3, orientacion: "V"}}/>
+            <SpaceShip state={{coordenada_X: 20, coordenada_Y: 70 ,largo: 2, orientacion: "H"}}/>
+            <SpaceShip state={{coordenada_X: 60, coordenada_Y: 10 ,largo: 2, orientacion: "V"}}/>
+            <SpaceShip />
+        </div>
       </div>
-      <div className="game-Board">
-          <SpaceShip state={{posicion_X: 20, posicion_Y: 20 ,largo: 3, orientacion: "Y"}}/>
-          <SpaceShip state={{posicion_X: 20, posicion_Y: 70 ,largo: 2, orientacion: "X"}}/>
-          <SpaceShip state={{posicion_X: 60, posicion_Y: 10 ,largo: 2, orientacion: "Y"}}/>
-          <SpaceShip />
-      </div>
-    </div>
     );
   }
 
--- buttleship-espacial/src/components/SpaceShip.js
+++ buttleship-espacial/src/components/SpaceShip.js
@@ -1,48 +1,54 @@
 import React, { Component } from 'react';
 
+const InitialState = {
+    coordenada_X: 0,
+    coordenada_Y: 0,
+    largo: 4,
+    orientacion: "H"
+}
+
+
 class SpaceShip extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            posicion_X: typeof this.props.state === 'undefined' ? 0 : this.props.state.posicion_X,    //0,
-            posicion_Y: typeof this.props.state === 'undefined' ? 0 : this.props.state.posicion_Y,
-            largo: typeof this.props.state === 'undefined' ? 4 : this.props.state.largo,
-            orientacion: typeof this.props.state === 'undefined' ? "X" : this.props.state.orientacion
-        }
+        // this.state = {
+        //     coordenada_X: this.props.state.coordenada_X,    //0,
+        //     coordenada_Y: this.props.state.coordenada_Y,
+        //     largo: this.props.state.largo,
+        //     orientacion: this.props.state.orientacion
+        // }
+        this.state = typeof this.props.state === 'undefined' ? InitialState : this.props.state;
         this.printSpaceShip = this.printSpaceShip.bind(this);
         ;
     }
     printSpaceShip(){
         console.log(this.props);
-        console.log(this.props.posicion_X);
-        console.log(this.props.posicion_Y);
+        console.log(this.props.coordenada_X);
+        console.log(this.props.coordenada_Y);
         console.log(this.props.largo);
         console.log(this.props.orientacion);
         console.log("---")
-        console.log(this.state.posicion_X);
-        console.log(this.state.posicion_Y);
+        console.log(this.state.coordenada_X);
+        console.log(this.state.coordenada_Y);
         console.log(this.state.largo);
         console.log(this.state.orientacion);
     }
     render() { 
         let array = [];
-        if( this.state.orientacion === "X" ){
-            for (var i = 0; i < this.state.largo; i++) {
-                // console.log("i: "+i);
+        for (var i = 0; i < this.state.largo; i++) {
+            // console.log("i: "+i);
+            if( this.state.orientacion === "H" ){
                 const style = {
-                    left: `${this.state.posicion_X+10*i}%`,
-                    top: `${this.state.posicion_Y}%`
+                    left: `${this.state.coordenada_X+10*i}%`,
+                    top: `${this.state.coordenada_Y}%`
                 }
                 array.push(
                     <div className="spaceship" key={i} style={style}></div>
                 )
-            }
-        }else if( this.state.orientacion === "Y" ){
-            for (i = 0; i < this.state.largo; i++) {
-                // console.log("i: "+i);
+            }else if( this.state.orientacion === "V" ){
                 const style = {
-                    left: `${this.state.posicion_X}%`,
-                    top: `${this.state.posicion_Y+10*i}%`
+                    left: `${this.state.coordenada_X}%`,
+                    top: `${this.state.coordenada_Y+10*i}%`
                 }
                 array.push(
                     <div className="spaceship" key={i} style={style}></div>

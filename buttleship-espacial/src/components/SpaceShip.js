1f2d92d34af955d6427f3bb6bfa09f2ac337494f
CarolinaMedin <carolina.medina@usach.cl>
2020-01-25 23:27:10 -0300

    agregando comentarios a clase SpaceShip sobre estructura React

--- buttleship-espacial/src/components/SpaceShip.js
+++ buttleship-espacial/src/components/SpaceShip.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const InitialState = {
+const DefaultState = {
     coordenada_X: 0,
     coordenada_Y: 0,
     largo: 4,
@@ -9,6 +9,8 @@ const InitialState = {
 
 
 class SpaceShip extends Component {
+    // El constructor es un método que se llama automáticamente durante la creación de un objeto desde una clase. Puede manejar sus cosas de configuración inicial, como el incumplimiento de algunas propiedades del objeto, o la comprobación de los argumentos que se pasaron. Simplemente, el constructor ayuda a construir cosas.
+    // Ya para la inicialización de estado, se define en el constructor, con lo que se ejecutará para cada componente instanciado de manera independiente.
     constructor(props) {
         super(props);
         // this.state = {
@@ -17,10 +19,33 @@ class SpaceShip extends Component {
         //     largo: this.props.state.largo,
         //     orientacion: this.props.state.orientacion
         // }
-        this.state = typeof this.props.state === 'undefined' ? InitialState : this.props.state;
+
+        // El constructor es el único lugar donde debe asignar el estado local directamente así. En cualquier otro lugar de nuestro componente, debe confiar en setState () en su lugar.
+        this.state = typeof this.props.state === 'undefined' ? DefaultState : this.props.state;        // El constructor de su componente es el lugar perfecto para establecer el estado inicial de su componente. En lugar de usar setState () como lo haría en otros métodos en su clase, deberá establecer el estado inicial directamente:
+        
+        // Recomendado: Vincula eventos en un solo lugar
         this.printSpaceShip = this.printSpaceShip.bind(this);
         ;
     }
+    // Podemos declarar métodos especiales en la clase del componente para ejecutar algún código cuando un componente se monta y desmonta:
+    // El método componentDidMount() se ejecuta después que la salida del componente ha sido renderizada en el DOM. Este es un buen lugar para configurar un temporizador:
+    componentDidMount() {
+        // this.timerID = setInterval(
+        //     () => this.tick(),
+        //     1000
+        // );
+    }
+    // Si el componente se elimina en algún momento del DOM, React invoca al método de ciclo de vida componentWillUnmount()
+    componentWillUnmount() {
+        // clearInterval(this.timerID);
+    }
+    
+    // tick() {
+    //     this.setState({ // Utilizará this.setState() para programar actualizaciones al estado local del componente.
+    //          date: new Date()
+    //     });
+    // }
+
     printSpaceShip(){
         console.log(this.props);
         console.log(this.props.coordenada_X);
@@ -63,4 +88,15 @@ class SpaceShip extends Component {
     }
 }
  
+// Una vez tenemos la clase, nos tenemos que preocupar de exportarla, para que se conozca desde fuera de este módulo. Esto se consigue con la sentencia "export" de ES6.
 export default SpaceShip;
+
+// la inicialización de propiedades se hace una vez definida la clase, mediante una propiedad que usamos dentro de la propia clase. Al no estar en el código de la clase, se ejecutará una única vez.
+SpaceShip.defaultProps = {
+    props: {
+        coordenada_X: 0,
+        coordenada_Y: 0,
+        largo: 4,
+        orientacion: "H"
+    }   // 'Valor por defecto definido para la propiedad'
+  }
